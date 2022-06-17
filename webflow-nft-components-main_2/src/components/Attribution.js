8ac8efe865685f0f213a6f0a3799b0914ecd0d9a
olivercrypto <106682550+olivercrypto@users.noreply.github.com>
2022-06-17 13:49:22 +0200

    Add files via upload

---
+++ webflow-nft-components-main_2/src/components/Attribution.js
@@ -0,0 +1,62 @@
+import { Box, } from '@mui/material';
+import { getBaseURL } from '../constants';
+import { useEffect, useState } from 'react';
+import { NFTContract } from '../contract';
+
+export const Attribution = (props) => {
+    const [attributionText, setAttributionText] = useState("Widget by Buildship")
+
+    useEffect(() => {
+        const updateAttribution = async () => {
+            try {
+                if (Object.keys(NFTContract.methods).includes("DEVELOPER")) {
+                    const developer = await NFTContract.methods.DEVELOPER().call()
+                    console.log(developer)
+                    if (developer.toLowerCase().includes("buildship")) {
+                        setAttributionText(_ => "Launched with Buildship")
+                    }
+                }
+            }
+            catch (e) {
+                console.log("Couldn't read contract developer")
+                console.log(e)
+            }
+        }
+
+        updateAttribution()
+    }, [])
+
+    return <Box
+        onClick={() => window.open("https://buildship.xyz")}
+        sx={{
+            mt: 4,
+            cursor: "pointer",
+            display: "flex",
+            alignItems: "center",
+            ...props?.sx
+        }}>
+        {/* for SEO */}
+        <a href="https://buildship.xyz" />
+        <div style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: 24,
+            height: 24,
+            borderRadius: 24,
+        }}>
+            <img
+                width={14}
+                src={`${getBaseURL()}/images/buildship${window.STYLES?.theme === "dark" ? "-white" : ""}.svg`}
+            />
+        </div>
+        <Box sx={{
+            marginLeft: "2px",
+            fontSize: 14,
+            fontWeight: 400,
+            color: (theme) => theme.palette.grey[500],
+        }}>
+            {attributionText}
+        </Box>
+    </Box>
+}
---
+++ webflow-nft-components-main_2/src/components/QuantityModalStep.js
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from 'react';
+import { Box, Button, Slider } from '@mui/material';
+import { getDefaultMaxTokensPerMint, getMaxSupply, getMaxTokensPerMint, getMintedNumber, mint } from '../mint/web3';
+import { showAlert } from './AutoHideAlert';
+import { parseTxError } from '../utils';
+import { Attribution } from './Attribution';
+
+export const QuantityModalStep = ({ setQuantity, setStep, setIsLoading, setTxHash }) => {
+    const [quantityValue, setQuantityValue] = useState(1)
+    const [maxTokens, setMaxTokens] = useState(getDefaultMaxTokensPerMint())
+    const [mintedNumber, setMintedNumber] = useState()
+    const [totalNumber, setTotalNumber] = useState()
+
+    useEffect(() => {
+        getMaxTokensPerMint().then(setMaxTokens)
+        if (!window.DEFAULTS?.hideCounter) {
+            getMintedNumber().then(setMintedNumber)
+            getMaxSupply().then(setTotalNumber)
+        }
+    }, [])
+
+    const step = maxTokens <= 5 ? maxTokens : 10
+    const marks = [...Array(Math.floor(maxTokens / step) + 1)]
+        .map((_, i) => 1 + i * step)
+        .map(m => ({
+            value: Math.max(1, m - 1),
+            label: (Math.max(1, m - 1)).toString()
+        }))
+
+    const onSuccess = async () => {
+        // if (window.CONTRACT.nft.allowedNetworks[0] === 137) {
+        //     setStep(2)
+        //     return
+        // }
+
+        setIsLoading(true)
+        const { tx } = await mint(quantityValue)
+        if (tx === undefined) {
+            setIsLoading(false)
+        }
+        tx?.on("transactionHash", (hash) => {
+            setTxHash(hash)
+        })?.on("confirmation", async () => {
+            setIsLoading(false)
+            showAlert(`Successfully minted ${quantityValue} NFTs`, "success")
+        })?.on("error", (e) => {
+            setIsLoading(false)
+            const { code, message } = parseTxError(e);
+            if (code !== 4001) {
+                showAlert(`Minting error: ${message}. Please try again or contact us`, "error");
+            }
+        })
+    }
+
+    return <div style={{ width: "100%" }}>
+        <Box sx={{
+            display: "flex",
+            alignItems: "flex-end",
+            width: "100%",
+            height: 75
+        }}>
+            <Slider
+                sx={{ ml: 2 }}
+                aria-label="Quantity"
+                defaultValue={1}
+                valueLabelDisplay="auto"
+                onChange={(e, v) => {
+                    setQuantity(v)
+                    setQuantityValue(v)
+                }}
+                step={1}
+                marks={marks}
+                min={1}
+                max={maxTokens}
+            />
+        </Box>
+        <Button
+            onClick={onSuccess}
+            sx={{ mt: 4, width: "100%" }}
+            variant="contained"
+        >
+            Mint now
+        </Button>
+        {!window.DEFAULTS?.hideCounter && <Box
+            sx={{
+                color: (theme) => theme.palette.grey[500],
+                display: "flex",
+                fontWeight: 400,
+                fontSize: 14,
+                justifyContent: "center",
+                mt: 2
+            }}>
+            <span>{mintedNumber ?? "-"}</span>
+            <span style={{ margin: "0 2px"}}>/</span>
+            <span>{totalNumber ?? "-"}</span>
+        </Box>}
+        <Attribution sx={{ mt: 3, justifyContent: "center" }} />
+    </div>
+}
